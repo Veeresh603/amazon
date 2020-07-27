107af205289c7b2a6e1ba04ed149ac804b3255da
Veeresh603 <57994727+Veeresh603@users.noreply.github.com>
2020-07-27 21:55:49 +0530

    hg

--- src/HeaderTwo.js
+++ src/HeaderTwo.js
@@ -1,15 +1,39 @@
 import React from 'react';
 import './headertwo.css';
+import Location from '@material-ui/icons/LocationCity';
+import { Link } from 'react-router-dom';
 
 
 function HeaderTwo() {
     return (
+        
         <nav className="headertwo">
+            <div className="location_icon">
+               <Location  /> 
+             </div>
              <div className="header_location">
+            
              <span className="headeroptionOne_location">Deliver To</span>
              <span className="headeroptionTwo_location">India</span>
-
+             
          </div>
+         <div className="offer_links"> 
+             <Link to="/" className="offeroption">
+              Today's Deals
+            </Link>
+            <Link to="/"  className="offeroption">
+             Customer Service
+            </Link> <Link to="/"  className="offeroption">
+            Gift Cards
+            </Link> <Link to="/"  className="offeroption">
+                Registry
+            </Link>
+            <Link to="/"  className="offeroption">
+                Sell
+            </Link>
+         </div>
+         
+         
         </nav>
     )
 }
