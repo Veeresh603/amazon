4eb9d3ec0bb85722d4ee3a4a21134d9e0e6d2e37
Veeresh603 <57994727+Veeresh603@users.noreply.github.com>
2020-07-29 00:21:45 +0530

    bann

--- src/CarouseL.js
+++ src/CarouseL.js
@@ -1,58 +1,43 @@
 import React from 'react';
-import Carousel from 'react-bootstrap/Carousel';
 import './C_arousel.css';
 
 function C_arousel() {
     return (
-        <div className="carousel_banner">
-        <Carousel>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="https://www.amazon.in/images/G/31/prime/Events/Pug/Leadup/GW/3000x1200_main_1x._CB409438475_.jpg"
-      alt="First slide"
-    />
-    <Carousel.Caption>
-     
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="https://www.amazon.in/images/G/31/img19/AmazonPay/Boson/Sid/UPI/July/DesktopHero_1500x600_IMC_Unrec._CB428573416_.jpg"
-      alt="Third slide"
-    />
-
-    <Carousel.Caption>
-     
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="https://www.amazon.in/images/G/31/img17/Biss/2020/GW/Herotator/1218154_1500x600_2._CB430975671_.jpg"
-      alt="Third slide"
-    />
-
-    <Carousel.Caption>
-     
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="https://www.amazon.in/images/G/31/img20/Wireless/Avicii/July/PreOrder/28th/Prebooknow/V214721272_IN_WLME_Avicii_LP_DesktopTallHero_1500x600_7._CB409789003_.jpg"
-      alt="Third slide"
-    />
-
-    <Carousel.Caption>
-     
-    </Carousel.Caption>
-  </Carousel.Item>
-  
-</Carousel>
-</div>     
+      <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel" interval="1000">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+        <img
+          className="d-block w-100"
+          src="https://www.amazon.in/images/G/31/prime/Events/Pug/Leadup/GW/3000x1200_main_1x._CB409438475_.jpg"
+          alt="First slide"
+        />
+       
+        </div>
+        <div class="carousel-item">
+        <img
+          className="d-block w-100"
+          src="https://www.amazon.in/images/G/31/img19/AmazonPay/Boson/Sid/UPI/July/DesktopHero_1500x600_IMC_Unrec._CB428573416_.jpg"
+          alt="Second slide"
+        />
+        </div>
+        <div class="carousel-item">
+        <img
+          className="d-block w-100"
+          src="https://www.amazon.in/images/G/31/img17/Biss/2020/GW/Herotator/1218154_1500x600_2._CB430975671_.jpg"
+          alt="Third slide"
+        />
+        </div>
+        <div class="carousel-item">
+        <img
+          className="d-block w-100"
+          src="https://www.amazon.in/images/G/31/img20/Wireless/Avicii/July/PreOrder/28th/Prebooknow/V214721272_IN_WLME_Avicii_LP_DesktopTallHero_1500x600_7._CB409789003_.jpg"
+          alt="Fourth slide"
+        />
+        </div>
+      </div>
+    </div>
     )
+    
 }
 
 export default C_arousel
--- src/Products.js
+++ src/Products.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Products.css';
+import { Link } from 'react-router-dom'
 
 function Products() {
     return (
@@ -8,24 +9,33 @@ function Products() {
                 <h1 className="titlename">Women's Home Wear Basics</h1>
             </div>
             <div className="insidebox">
+                
                 <div className="imagebox">
+                <Link to="/" className="Productfirstlink">
                 <img  className="image" src="https://www.amazon.in/images/G/31/img20/GW-BasicsForMenWomen/1_1X._SY116_CB430251838_.jpg" alt=""></img>
                 <h4 className="ethnic">Everyday Ethnic Wear</h4>
+                </Link>
                 </div>
                 <div className="imageboxone">
+                <Link to="/" className="Productfirstlink">
                 <img  className="image" src="https://www.amazon.in/images/G/31/img20/GW-BasicsForMenWomen/2_1X._SY116_CB430251832_.jpg" alt=""></img>
                 <h4 className="ethnic">Comfy tops & tees</h4>
+                </Link>
                 </div>
 
             </div>
             <div className="insidebox">
                 <div className="imagebox">
+                <Link to="/" className="Productfirstlink">
                 <img className="image" src="https://www.amazon.in/images/G/31/img20/GW-BasicsForMenWomen/3_1X._SY116_CB430251832_.jpg" alt=""></img>
                 <h4 className="ethnic">Loungewear</h4>
+                </Link>
                 </div>
                 <div className="imageboxone">
+                <Link to="/" className="Productfirstlink">
                 <img  className="image" src="https://www.amazon.in/images/G/31/img20/GW-BasicsForMenWomen/4_1X._SY116_CB430251832_.jpg" alt=""></img>
                 <h4 className="ethnic">Flipflops</h4>
+                </Link>
                 </div>
 
             </div>
