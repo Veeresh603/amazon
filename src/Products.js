bb90fe3ebdc716fc86a38d384d1e2a4c099a1e09
Veeresh603 <57994727+Veeresh603@users.noreply.github.com>
2020-07-28 17:35:33 +0530

    five

--- src/Products.js
+++ src/Products.js
@@ -14,16 +14,18 @@ function Products() {
                 </div>
                 <div className="imageboxone">
                 <img  className="image" src="https://www.amazon.in/images/G/31/img20/GW-BasicsForMenWomen/2_1X._SY116_CB430251832_.jpg" alt=""></img>
-                <h4 className="ethnic">Everyday Ethnic Wear</h4>
+                <h4 className="ethnic">Comfy tops & tees</h4>
                 </div>
 
             </div>
             <div className="insidebox">
                 <div className="imagebox">
                 <img className="image" src="https://www.amazon.in/images/G/31/img20/GW-BasicsForMenWomen/3_1X._SY116_CB430251832_.jpg" alt=""></img>
+                <h4 className="ethnic">Loungewear</h4>
                 </div>
                 <div className="imageboxone">
                 <img  className="image" src="https://www.amazon.in/images/G/31/img20/GW-BasicsForMenWomen/4_1X._SY116_CB430251832_.jpg" alt=""></img>
+                <h4 className="ethnic">Flipflops</h4>
                 </div>
 
             </div>
