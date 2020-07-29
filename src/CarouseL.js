bbf2329dc1d8b7fb12f4250e2227e62cde8bba21
Veeresh603 <57994727+Veeresh603@users.noreply.github.com>
2020-07-29 17:43:34 +0530

    djjfdlf

--- src/CarouseL.js
+++ src/CarouseL.js
@@ -13,21 +13,21 @@ function C_arousel() {
         />
        
         </div>
-        <div class="carousel-item">
+        <div class="carousel-item active">
         <img
           className="d-block w-100"
           src="https://www.amazon.in/images/G/31/img19/AmazonPay/Boson/Sid/UPI/July/DesktopHero_1500x600_IMC_Unrec._CB428573416_.jpg"
           alt="Second slide"
         />
         </div>
-        <div class="carousel-item">
+        <div class="carousel-item active">
         <img
           className="d-block w-100"
           src="https://www.amazon.in/images/G/31/img17/Biss/2020/GW/Herotator/1218154_1500x600_2._CB430975671_.jpg"
           alt="Third slide"
         />
         </div>
-        <div class="carousel-item">
+        <div class="carousel-item active">
         <img
           className="d-block w-100"
           src="https://www.amazon.in/images/G/31/img20/Wireless/Avicii/July/PreOrder/28th/Prebooknow/V214721272_IN_WLME_Avicii_LP_DesktopTallHero_1500x600_7._CB409789003_.jpg"
